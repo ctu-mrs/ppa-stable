0754350edd18ec30be5c366346b86c83f3318f32
Filip Stojanović <stojafil@fel.cvut.cz>
2026-08-05 12:31:23 +0200

    Shorten flags

--- resources/scripts/add_ros_ppa.sh
+++ resources/scripts/add_ros_ppa.sh
@@ -7,15 +7,15 @@ trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
 echo "$0: Adding ROS PPA"
 
-sudo apt-get -o Acquire::Retries="4" -y install wget lsb-release gnupg curl sudo
+sudo apt-get -o Acquire::Retries=4 -y install wget lsb-release gnupg curl sudo
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-curl -s --retry 4 --retry-max-time 60 --retry-connrefused https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+curl -s --retry 4 --retry-connrefused https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
-sudo apt-get -o Acquire::Retries="4" update
+sudo apt-get -o Acquire::Retries=4 update
 
-sudo apt-get -o Acquire::Retries="4" -y install python3-pip
+sudo apt-get -o Acquire::Retries=4 -y install python3-pip
 
 sudo pip3 install -U rosdep
 
--- resources/scripts/add_sources_ppa.sh
+++ resources/scripts/add_sources_ppa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -s --compressed --retry 4 --retry-max-time 60 --retry-connrefused "https://ctu-mrs.github.io/ppa-stable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg > /dev/null
-sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-connrefused -o /etc/apt/sources.list.d/ctu-mrs-stable.list "https://ctu-mrs.github.io/ppa-stable/ctu-mrs-apt.list"
-sudo curl -s --compressed --retry 4 --retry-max-time 60 --retry-connrefused -o /etc/apt/preferences.d/ctu-mrs-stable-preferences "https://ctu-mrs.github.io/ppa-stable/ctu-mrs-ppa-preferences.txt"
+curl -s --compressed --retry 4 --retry-connrefused "https://ctu-mrs.github.io/ppa-stable/ctu-mrs.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ctu-mrs.gpg > /dev/null
+sudo curl -s --compressed --retry 4 --retry-connrefused -o /etc/apt/sources.list.d/ctu-mrs-stable.list "https://ctu-mrs.github.io/ppa-stable/ctu-mrs-apt.list"
+sudo curl -s --compressed --retry 4 --retry-connrefused -o /etc/apt/preferences.d/ctu-mrs-stable-preferences "https://ctu-mrs.github.io/ppa-stable/ctu-mrs-ppa-preferences.txt"
 
-sudo apt-get -o Acquire::Retries="4" update
+sudo apt-get -o Acquire::Retries=4 update
