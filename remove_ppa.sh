0b72ffa08187b13cc2cc9173cd4cbc75ac92e424
cturmrsbot <cturmrsbot@users.noreply.github.com>
2026-09-15 16:38:51 +0000

    deploy: 48bf1c480d465f8c178af737a031a0d45ddcc951

---
+++ remove_ppa.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "$0: Removing MRS Stable PPA repository"
+
+sudo rm /etc/apt/sources.list.d/ctu-mrs-stable.list
+sudo rm /etc/apt/preferences.d/ctu-mrs-stable-preferences
+sudo rm /etc/ros/rosdep/sources.list.d/ctu-mrs-stable.list
+
+sudo apt-get -o Acquire::Retries=4 update
+rosdep --include-eol-distros --rosdistro=noetic update
+
+echo "$0: Finished removing MRS Stable PPA repository"
